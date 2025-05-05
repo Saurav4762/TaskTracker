2b0c4bcf550e2b0aad2d700d20b2e8f795ec68ee
saurav4762 <upretysaurav4@gamil.com>
2025-05-05 09:00:48 +0545

    feat: Added Entities

---
+++ Entities/Assignment.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualBasic;
+
+namespace TaskTracker.Entities;
+
+public class Assignment
+{
+    public Guid Id { get; set; }
+    
+    public string Title { get; set; }
+    
+    public string Description { get; set; }
+    
+    public DateTime DueDate { get; set; }
+    
+    public bool IsSubmitted { get; set; }
+    
+    public Guid StudentId { get; set; }
+    
+    public virtual Student Student { get; set; }
+}
---
+++ Entities/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskTracker.Entities;
+
+public class Student
+{
+    public Guid Id { get; set; }
+    
+    public string FullName { get; set; }
+    
+    public string Email { get; set; }
+    
+    public ICollection<Assignment> Assignments { get; set; }
+}
